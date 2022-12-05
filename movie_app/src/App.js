18ba36912901c42123e882dfda083dd34925d191
Saad575757 <smartysaad59@gmail.com>
2022-12-05 19:14:49 +0500

    ok

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Home from './Home';
+import Movie from './Movie';
 import Singlemovie from './Singlemovie';
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <>
     <Home />
     <Singlemovie />
+    <Movie />
     </>
   )
 }
