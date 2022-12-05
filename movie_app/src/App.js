a526c07d50c01eb100f63b2073436e85b616e2a6
Saad575757 <smartysaad59@gmail.com>
2022-12-05 19:58:14 +0500

    ok

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,18 +2,20 @@ import React from 'react'
 import Home from './Home';
 // import Movie from './Movie';
 import Singlemovie from './Singlemovie';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error from './Error';
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-    <BrowserRouter>
+    
     <Routes>
           <Route path="/" element={<Home />} />
           <Route path='movie/:id' element={<Singlemovie />} />
+          <Route  path="*" element={<Error />} />
           
     </Routes>
-    </BrowserRouter>
+    
     </>
   )
 }
--- movie_app/src/Singlemovie.js
+++ movie_app/src/Singlemovie.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const Singlemovie = () => {
+  const {id}= useParams()
   return (
-    <div>Singlemovie</div>
+    <>
+    <div>
+      Hello Movie {id}
+    </div>
+    </>
   )
 }
 
