23742ba923977e5648542086327a18aff9902621
Saad575757 <smartysaad59@gmail.com>
2022-12-08 00:22:38 +0500

    oo

--- movie_app/src/Context.js
+++ movie_app/src/Context.js
@@ -1,13 +1,13 @@
 import React, { useContext } from "react";
 
-const AppContext =React.createContext(); //context
+const AppContext = React.createContext(); //context
 //Provider
-const AppProvider=({children})=>{
+const AppProvider = ({ children }) => {
     return <AppContext.Provider value="Saad">{children}</AppContext.Provider>
 }
 
-const useGlobalcontext=()=>{
-  return useContext(AppContext);
+const useGlobalcontext = () => {
+    return useContext(AppContext);
 }
 
-export {AppContext, AppProvider, useGlobalcontext};
+export { AppContext, AppProvider, useGlobalcontext };
