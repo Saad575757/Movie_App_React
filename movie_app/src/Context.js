24662f4585b60519165c8dda55c12b9b23868e09
Saad575757 <smartysaad59@gmail.com>
2022-12-09 01:32:37 +0500

    okkk

--- movie_app/src/Context.js
+++ movie_app/src/Context.js
@@ -1,9 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 const AppContext = React.createContext(); //context
 //Provider
 const AppProvider = ({ children }) => {
-    return <AppContext.Provider value="Saad">{children}</AppContext.Provider>
+    const API_URL = "https://omdbapi.com/?apikey=727bbdc1&s=titanic";
+
+    const[isLoading,setisLoading]=useState(true);
+    const[movie,setMovie]=useState([]);
+    const[isError, setisError]=useState({show: "false", msg:""});
+    
+    const getMovies=async(url)=>{
+        try{
+            const res= await fetch(url);
+            const data = await res.json();
+
+            console.log(data);
+
+            if(data.Reponse==='true'){
+                setisLoading(false);
+                setMovie(data.Search);
+            }
+        }catch(error){
+            console.log(error);
+        }
+
+    }
+    useEffect(()=>{
+        getMovies(API_URL);
+    },[]);
+
+    return <AppContext.Provider value="Saadkhan">{children}</AppContext.Provider>
 }
 
 const useGlobalcontext = () => {
