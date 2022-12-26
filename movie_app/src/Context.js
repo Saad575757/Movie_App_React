e0fabc58a73d62529bea064cb1e90703a32c6747
Saad575757 <smartysaad59@gmail.com>
2022-12-26 17:44:04 +0500

    Great

--- movie_app/src/Context.js
+++ movie_app/src/Context.js
@@ -41,7 +41,7 @@ const AppProvider = ({ children }) => {
     const[isLoading,setisLoading]=useState(true);
     const[movie,setMovie]=useState([]);
     const[isError,setisError ]=useState({show:'false', msg:''});
-    const[querry,setQuerry]=useState('titanic');
+    const[querry,setQuerry]=useState('Batman');
 
     const getMovies= async(url)=>{
         
@@ -69,7 +69,11 @@ const AppProvider = ({ children }) => {
     } 
 
     useEffect(()=>{
-        getMovies(`${API_URL}&s=${querry}`);
+        let timerOut=setTimeout(() => {
+            getMovies(`${API_URL}&s=${querry}`);
+        }, 1000);
+        return ()=> clearTimeout(timerOut);
+       
     },[querry]);
 
     return <AppContext.Provider value={{movie,isError,isLoading, setQuerry}}>{children}</AppContext.Provider>
